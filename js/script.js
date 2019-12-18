b724a97b74828c76e12ed00742cc9f25432379ce
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-18 15:22:21 +0100

    Added travel and age

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 // welcome screen
-alert("Welcome to Trenitalia, fellow user.")
+alert("Welcome to Trenitalia, fellow user.");
 
 // how much km do you want to travel?
 var travelKm = parseInt(prompt("How much distant would you like to travel today?"));
-console.log(travelKm);
+if (isNaN(travelKm)) {
+  travelKm = parseInt(prompt("Please just insert a numeric value without any symbol for the travel distance."));
+  if (isNaN(travelKm)) {
+    alert("Sorry, I can't write loops just yet, reload the page and write normal numbers, thank you.");
+    throw new Error("User didn't set an ordinary numeric value.");
+  }
+}
+console.log("travelKM set to " + travelKm);
 
 // how old are you?
 var travelAge = parseInt(prompt("How old are you?"));
-console.log(travelAge);
+if (isNaN(travelAge)) {
+  travelAge = parseInt(prompt("Please just insert a numeric value without any symbol for the age."));
+  if (isNaN(travelAge)) {
+    alert("Sorry, I can't write loops just yet, reload the page and write normal numbers, thank you.");
+    throw new Error("User didn't set an ordinary numeric value.");
+  }
+}
+console.log("travelAge set to " + travelAge);
 
 // then the total price of your ticket is...
 // 0.21 euros per km
