c4c5a3ff2851b4e2f149f41be0825383b4245deb
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-18 14:59:59 +0100

    Added JS structure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// welcome screen
+alert("Welcome to Trenitalia, fellow user.")
+
+// how much km do you want to travel?
+var travelKm = parseInt(prompt("How much distant would you like to travel today?"));
+console.log(travelKm);
+
+// how old are you?
+var travelAge = parseInt(prompt("How old are you?"));
+console.log(travelAge);
+
+// then the total price of your ticket is...
+// 0.21 euros per km
+// < 18y 20% discount
+// > 65y 40% discount
+
+// var test = Math.floor(Math.random() * 100) + 1;
+// console.log(test);
